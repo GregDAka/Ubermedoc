1aee2725ca4a77bd3da17245ffd143df3c81305b
Biioz <dufour.b3012@gmail.com>
2024-04-25 23:53:01 +0200

    class obj v2 (Commande)

--- UberMedoc/commande.cpp
+++ UberMedoc/commande.cpp
@@ -1,3 +1,34 @@
 #include "commande.h"
+#include <vector>
+#include <iostream>
+using namespace std;
 
-Commande::Commande() {}
+Commande::Commande(){}
+
+Commande::~Commande(){};
+
+void Commande::afficherDetails(){
+    if(!articles.empty()){
+        for (Medicament* medoc : articles){
+            medoc->afficherDetails();
+            cout << "--------------------------------"<<endl;
+        }
+    }else{
+        cout << "La commande est vide :/" << endl;
+    }
+    cout <<  "-> " << total << endl;
+}
+
+void Commande::ajouterMedicament(Medicament* medoc){
+    articles.push_back(medoc);
+}
+
+void Commande::retirerMedicament(Medicament* medoc){
+    int i = 0;
+    for (Medicament* med : articles){
+        if (med->getRef() == medoc->getRef()){
+            articles.erase(articles.begin() + i);
+        }
+        i++;
+    }
+}
--- UberMedoc/commande.h
+++ UberMedoc/commande.h
@@ -1,10 +1,24 @@
+#include <vector>
+#include "medicament.h"
+#include "client.h"
+using namespace std;
 #ifndef COMMANDE_H
 #define COMMANDE_H
 
 class Commande
 {
-public:
-    Commande();
+private:
+    vector<Medicament*> articles;
+    double total;
+    Client client;
+
+    public:
+        Commande();
+        ~Commande();
+        void afficherDetails();
+        void ajouterMedicament(Medicament*);
+        void retirerMedicament(Medicament*);
+        double getTotal();
 };
 
 #endif // COMMANDE_H
