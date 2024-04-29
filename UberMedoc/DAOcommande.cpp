03d984b30c821fc2974a7090ba2918cec3f1b100
GabrielCodeY <gabriel.delemasure@gmail.com>
2024-04-29 18:50:29 +0200

    Création de la classe DAOcommande (non implémenter).

---
+++ UberMedoc/DAOcommande.cpp
@@ -0,0 +1,5 @@
+#include "DAOcommande.h"
+
+void DAOcommande::recupCommande(){
+
+}
---
+++ UberMedoc/DAOcommande.h
@@ -0,0 +1,11 @@
+#ifndef DAOCOMMANDE_H
+#define DAOCOMMANDE_H
+
+// NE PAS USING NAMESPACE STD DANS LE .H
+class DAOcommande{
+
+    void recupCommande() ;
+
+};
+
+#endif // DAOCOMMANDE_H
